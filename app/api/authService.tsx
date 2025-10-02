5efdce695594744ed4b7daa124c89ded53552705
Gaelle-M <gaelle-mazzali@hotmail.fr>
2025-10-02 20:10:32 +0200

    mise en place de la connexion

---
+++ app/api/authService.tsx
@@ -0,0 +1,37 @@
+const API_BASE_URL = 'http://localhost:3001/api/v1';
+
+export const login = async (email : string, password : string) => {
+
+    const url = `${API_BASE_URL}/user/login`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+       
+            body: JSON.stringify({email, password})
+        });
+
+        if (!response.ok) {
+         
+            const errorData = await response.json();
+            throw new Error(errorData.message || 'E-mail ou mot de passe incorrect.');
+        }
+
+        const data = await response.json();
+
+        if (data.body && data.body.token) {
+            return {
+                token: data.body.token,
+                userName: data.body.userName || 'Utilisateur',
+            };
+        } else {
+            throw new Error('Token manquant dans la réponse de l’API.');
+        }
+
+    } catch (error) {
+        throw error;
+    }
+};
--- app/store/authSlice.tsx
+++ app/store/authSlice.tsx
@@ -2,41 +2,37 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  // Définition de l'état initial pour l'authentification
   isAuthenticated: false,
   token: null,
   user: {
     userName: '',
-    // Ajoutez d'autres infos si nécessaire, comme le prénom, le nom, etc.
   },
   error: null,
 };
 
 const authSlice = createSlice({
-  name: 'auth', // Nom utilisé pour préfixer les actions
+  name: 'auth', 
   initialState,
   reducers: {
-    // 1. Reducer pour la CONNEXION (succès)
+    // Reducer pour la CONNEXION 
     signInSuccess: (state, action) => {
       state.isAuthenticated = true;
       state.token = action.payload.token;
-      state.user.userName = action.payload.userName; // Stocke le nom d'utilisateur
+      state.user.userName = action.payload.userName; 
       state.error = null;
     },
-    // 2. Reducer pour la DÉCONNEXION
+    // Reducer pour la DÉCONNEXION
     signOut: (state) => {
-      // On réinitialise l'état au départ
+  
       return initialState; 
     },
-    // 3. Reducer pour la MISE À JOUR du nom
+    // Reducer MISE À JOUR du nom
     updateUserName: (state, action) => {
-      state.user.userName = action.payload; // action.payload contiendra le nouveau nom
+      state.user.userName = action.payload; 
     },
   },
 });
 
-// RTK génère automatiquement les actions pour vous !
 export const { signInSuccess, signOut, updateUserName } = authSlice.actions;
 
-// On exporte le reducer qui sera utilisé dans store.ts
 export default authSlice.reducer;
